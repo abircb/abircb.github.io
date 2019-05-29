f2392a1372bbcd1fb3bc15bc03bd189f2bcaff6c
abir <abirchandrabhushan@gmail.com>
2019-05-29 18:50:04 +0530

    Created load page

---
+++ js/load.js
@@ -0,0 +1,22 @@
+function load() {
+  var elem = document.getElementById("myBar");
+  var width = 10;
+  var id = setInterval(frame, 10);
+
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      ready();
+    }
+    else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+
+}
+
+function ready() {
+  document.getElementById("loading-area").style.opacity = '0';
+}
