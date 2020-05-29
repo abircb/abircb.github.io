b7eb646f543c32bbea42a040eb3c5dd0f77cf03e
abir <abirchandrabhushan@gmail.com>
2020-05-29 15:46:10 +0530

    setup config

---
+++ nouveau/abirbhushan.com/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: [
+      'plugin:vue/vue3-recommended'
+    ],
+    rules: {},
+    parserOptions: {
+        parser: 'babel-eslint',
+    }
+  }
---
+++ nouveau/abirbhushan.com/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  vueIndentScriptAndStyle: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
